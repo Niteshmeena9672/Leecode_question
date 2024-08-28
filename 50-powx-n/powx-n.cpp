69a6153ef13ac53f29f3b7104c041fbfb2652ee2
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-08-28 15:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (60.96%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     // helper function 
-    double power(double x , int n){
+    double power(double x , long n){
         // base case 
-
+        if( n== 0){
+            return 1.0;
+        };
         if(n==1) {
             return x;
         };
-        if( n== 0){
-            return 1.0;
-        }
+        
 
         if(n%2 != 0){
-            return x * power(x*x , n/2);
+            return x * power(x , n-1);
         }
         else {
             return power(x*x , n/2);
         }
     }
     double myPow(double x, int n) {
+            double num = n;
             if(n< 0){
-                return (1.0/power(x,n));
+                return (1.0/power(x,-num));
             }
             else{
-                return power(x,n);
+                return power(x,num);
             }
     }
 };
