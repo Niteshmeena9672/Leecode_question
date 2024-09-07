945b4bb9bb3007067eb4e6f6d4e5e31638aa6443
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-09-07 13:35:34 +0530

    Time: 13 ms (22.97%) | Memory: 16.3 MB (90.87%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -8,52 +8,82 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        stack<ListNode*> st;
-        ListNode* temp = head;
-        ListNode* ans = new ListNode();
-        ListNode* dummy = ans;
-
-        int ind = k;
-
-        ListNode* cur = head;
-        int cnt = 0;
-        while (cur) {
-            cnt++;
-            cur = cur->next;
+    /**
+     * Function to reverse the entire linked list.
+     * @param head: Pointer to the head of the linked list.
+     * @return New head of the reversed linked list.
+     */
+    ListNode* reverseLinkedList(ListNode *head) {
+        ListNode* temp = head;  // Initialize current node to head
+        ListNode* prev = NULL;  // Initialize previous node to NULL
+        
+        // Traverse through the list and reverse the links
+        while(temp != NULL) {
+            ListNode* front = temp->next;  // Store next node
+            temp->next = prev;             // Reverse the current node's pointer
+            prev = temp;                   // Move 'prev' one step forward
+            temp = front;                  // Move 'temp' one step forward
         }
+        return prev;  // Return the new head of the reversed list
+    }
 
+    /**
+     * Function to get the k-th node from the current position in the list.
+     * @param temp: Current node pointer.
+     * @param k: Position to move to from the current node.
+     * @return Pointer to the k-th node or NULL if it doesn't exist.
+     */
+    ListNode* getKthNode(ListNode* temp, int k) {
+        k -= 1;  // Since we are starting from the current node, reduce k by 1
+        while(temp != NULL && k > 0) {
+            k--;
+            temp = temp->next;  // Move to the next node
+        }
+        return temp;  // Return the k-th node or NULL
+    }
 
-        while(cnt >= k){
-            int remaining = k;
-            while (remaining > 0 && temp) {
-                st.push(temp);
-                cout << temp -> val;
-                temp = temp->next;
-                remaining--;
+    /**
+     * Function to reverse every k nodes in the linked list.
+     * @param head: Pointer to the head of the linked list.
+     * @param k: Number of nodes to reverse in each group.
+     * @return The new head of the modified linked list.
+     */
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;      // Initialize temp to the head of the list
+        ListNode* prevLast = NULL;  // Pointer to the last node of the previous reversed group
+        
+        // Iterate through the list and reverse k nodes at a time
+        while(temp != NULL) {
+            ListNode* kThNode = getKthNode(temp, k);  // Get the k-th node
+
+            // If less than k nodes remain, no reversal is done
+            if(kThNode == NULL) {
+                if(prevLast) {
+                    prevLast->next = temp;  // Connect the last processed node
+                }
+                break;  // Break the loop as no more reversals are needed
             }
             
-            while (!st.empty()) {
-                ListNode* node = st.top();
-                st.pop();
-                dummy->next = node; 
-                dummy = dummy->next;
+            ListNode* nextNode = kThNode->next;  // Store the next node after k-th
+            kThNode->next = NULL;  // Temporarily break the list
+            
+            // Reverse the current k-group
+            reverseLinkedList(temp);
+
+            // If this is the first group, update the head to the new head
+            if(temp == head) {
+                head = kThNode;
+            } else {
+                prevLast->next = kThNode;  // Connect the last group to the new reversed group
             }
-            cnt -= k;
+            
+            prevLast = temp;  // Move prevLast to the current group
+            temp = nextNode;  // Move temp to the next part of the list to continue the process
         }
         
-        dummy -> next = temp;
-
-        return ans -> next;
+        return head;  // Return the new head of the list
     }
 };
-
-
-int init = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
