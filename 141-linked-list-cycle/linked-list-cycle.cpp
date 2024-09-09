859f1ee7c548c226b883eae615b5c6097c16d7a0
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-09-09 14:27:46 +0530

    Time: 8 ms (70.42%) | Memory: 10.7 MB (96.04%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
+        // Check if the list is empty or has only one node
         if (head == NULL || head->next == NULL) {
-            return false;
+            return false; // No cycle in an empty or single-node list
         }
+
+        // Initialize two pointers: slow and fast
         ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != slow) {
+        ListNode* fast = head;
+
+        // Iterate until fast and slow meet
+        do{
+            // Check if fast has reached the end or its next node
             if (fast->next == NULL || fast->next->next == NULL) {
-                return false;
+                return false; // No cycle if fast reaches the end
             }
+
+            // Advance slow and fast at different speeds
             slow = slow->next;
             fast = fast->next->next;
-        }
+        }while (fast != slow);
+
+        // If fast and slow meet, a cycle exists
         return true;
     }
 };
