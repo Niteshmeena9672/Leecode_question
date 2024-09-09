872676b9c4c575a3c4a9a7e1ef8966494ea06db5
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-09-09 13:45:14 +0530

    Time: 8 ms (15.15%) | Memory: 14.9 MB (53.17%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx = -1;
-        int n = nums.size();
-        for(int i = n-2 ; i >= 0; i--){
-            if(nums[i]<nums[i+1]){
+        int idx = -1;  // Initialize idx to -1 to indicate no valid index found yet
+        int n = nums.size();  // Get the size of the array
 
-            idx = i;
-            break;
+        // Find the first index from the end where the current element is smaller than the next one
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                idx = i;  // Store the index
+                break;  // Exit loop as soon as the first such index is found
             }
         }
-        if(idx==-1){
-            reverse(nums.begin(), nums.end());
-        }
-        else {
 
-            for(int i = n-1 ; i>idx; i--){
-                if(nums[i]>nums[idx]){
-                    swap(nums[i],nums[idx]);
-                    reverse(nums.begin()+idx+1 , nums.end());
-                    break;
+        if(idx == -1){
+            // If no such index is found, the array is in descending order
+            // Reverse the entire array to get the smallest permutation
+            reverse(nums.begin(), nums.end());
+        } else {
+            // Find the first element from the end that is greater than nums[idx]
+            for(int i = n-1; i > idx; i--){
+                if(nums[i] > nums[idx]){
+                    // Swap the elements at idx and i
+                    swap(nums[i], nums[idx]);
+
+                    // Reverse the subarray to the right of idx to get the next permutation
+                    reverse(nums.begin() + idx + 1, nums.end());
+                    break;  // Exit loop once the swap and reverse are done
                 }
             }
         }
