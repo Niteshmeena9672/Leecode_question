d8a048962b076932997d058374fb1fccee64ce30
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-10-07 12:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (46.51%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int> &house,vector<int>&dp, int curr_index) {
-        if (curr_index >= house.size() )
-            return 0;
-        if(dp[curr_index]!=-1){
-            return dp[curr_index];
+    // int helper(vector<int> &house,vector<int>&dp, int curr_index) {
+    //     if (curr_index >= house.size() )
+    //         return 0;
+    //     if(dp[curr_index]!=-1){
+    //         return dp[curr_index];
+    //     }
+    //     int steal = house[curr_index] + helper(house,dp, curr_index + 2);
+    //     int skip = helper(house,dp, curr_index + 1);
+    //     return dp[curr_index] = max(steal, skip);
+    // }
+    int tabulation(vector<int> &house, vector<int>& dp) {
+        dp[0] = 0;
+        int n = house.size();
+        dp[1] = house[0];
+        for (int i = 2; i <= n; i++) {
+            int steal = house[i-1] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = max(steal, skip);
         }
-        int steal = house[curr_index] + helper(house,dp, curr_index + 2);
-        int skip = helper(house,dp, curr_index + 1);
-        return dp[curr_index] = max(steal, skip);
+        return dp[house.size()];
     }
 
         int rob(vector<int> & nums) {
-            vector<int>dp(101,-1);
-            return helper(nums,dp, 0);
+            vector<int> dp(101, -1);
+            return tabulation(nums, dp);
         }
     };
