8f9eba3d1447ab7ef915b5f77f3798582b8aaa1e
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-10-14 19:02:12 +0530

    Time: 111 ms (71.86%) | Memory: 74.7 MB (33.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    int longestConsecutiveSubsequence(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        int n = nums.size();
-        
-        // Mark all elements in the map as not visited
-        for (int i = 0; i < n; i++) {
-            mpp[nums[i]] = 0;
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>st;
+        for (auto num :nums){
+            st.insert(num);
         }
-        
         int longest_sequence = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (mpp[nums[i]] != 1) {
+        for(auto num : st){
+            if(st.find(num-1)== st.end()){
                 int curr_sequence = 0;
-                int starting_number = nums[i];
-                
-                // Find the smallest starting number of the sequence
-                while (mpp.find(starting_number - 1) != mpp.end()) {
-                    starting_number -= 1;
+                while(st.find(num)!= st.end()){
+                    num+=1;
+                    curr_sequence+=1;
                 }
-                
-                // Count the length of the sequence starting from the smallest number
-                while (mpp.find(starting_number) != mpp.end()) {
-                    curr_sequence += 1;
-                    mpp[starting_number] = 1; // Mark as visited
-                    starting_number += 1;
-                }
-                
-                longest_sequence = max(curr_sequence, longest_sequence);
+                longest_sequence = max(curr_sequence,longest_sequence);
             }
         }
-        
         return longest_sequence;
-    }
-
-    int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        return longestConsecutiveSubsequence(nums);
     }
 };
