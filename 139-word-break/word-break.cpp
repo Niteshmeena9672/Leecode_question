14e9b3383af8149f36bcc7d949b7894103bc60f8
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-10-14 17:16:13 +0530

    Time: 16 ms (34.11%) | Memory: 16.8 MB (35.57%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,60 @@
+// class Solution {
+// public:
+//     unordered_set<string> st; // Using an unordered set for O(1) lookup.
+
+//     bool solve(int idx, string &s) {
+//         int n = s.length();
+//         if (idx >= n) {
+//             return true;  // Base case: If the index reaches the end of the string, return true.
+//         }
+
+//         for (int l = 1; l <= n - idx; l++) {  // Check for every substring from current index.
+//             string temp = s.substr(idx, l);  // Create substring starting from idx with length l.
+
+//             // If the substring is in the set and we can solve the remaining part recursively.
+//             if (st.find(temp) != st.end() && solve(idx + l, s)) {
+//                 return true;
+//             }
+//         }
+
+//         return false;  // If no valid break point is found, return false.
+//     }
+
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         for (auto &word : wordDict) {
+//             st.insert(word);  // Store all words from wordDict in an unordered set for O(1) lookups.
+//         }
+
+//         return solve(0, s);  // Start the recursive solution from the beginning of the string.
+//     }
+// };
+class Solution {
+public:
+    unordered_set<string> st; // Using an unordered set for O(1) lookup.
+
+    bool solve(int idx, string &s,vector<int>&memo) {
+        int n = s.length();
+        if (idx >= n) {
+            return true;  // Base case: If the index reaches the end of the string, return true.
+        }
+        if(memo[idx]!=-1) return memo[idx];
+        for (int l = 1; l <= n - idx; l++) {  // Check for every substring from current index.
+            string temp = s.substr(idx, l);  // Create substring starting from idx with length l.
+
+            // If the substring is in the set and we can solve the remaining part recursively.
+            if (st.find(temp) != st.end() && solve(idx + l, s, memo)) {
+                return memo[idx]= true;
+            }
+        }
+
+        return memo[idx] = false;  // If no valid break point is found, return false.
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for (auto &word : wordDict) {
+            st.insert(word);  // Store all words from wordDict in an unordered set for O(1) lookups.
+        }
+        vector<int>memo(301,-1);
+        return solve(0, s,memo);  // Start the recursive solution from the beginning of the string.
+    }
+};
