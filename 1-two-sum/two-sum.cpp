176ad292dad3ed5e358668de01c156ffee1928ac
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-10-16 17:56:47 +0530

    Time: 7 ms (85.36%) | Memory: 16 MB (8.01%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
+    int n = nums.size();
+    unordered_map<int, int> mpp; // Hash map to store value and its index
 
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement)) {
-                return {numMap[complement], i};
-            }
-            numMap[nums[i]] = i;
-        }
-
-        return {}; // No solution found
+    for (int i = 0; i < n; i++) {
+        mpp[nums[i]] = i; // Store the index of each number
     }
+
+    for (int i = 0; i < n; i++) {
+        int remaining_value = target - nums[i];
+        // Check if the remaining value exists and its index is not the same as the current index
+        if (mpp.find(remaining_value) != mpp.end() && mpp[remaining_value] != i) {
+            return {i, mpp[remaining_value]}; // Return the indices
+        }
+    }
+    return {-1, -1}; // Return {-1, -1} if no solution is found
+}
 };
