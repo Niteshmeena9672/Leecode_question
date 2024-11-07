e698b21450dbdcdc60705216207cab0da985482c
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-11-07 13:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (95.34%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+
+     int helper (vector<int>&house , int curr_index, vector<int>&dp){
+        if(curr_index>= house.size()) return 0;
+        if(dp[curr_index]!=-1) return dp[curr_index];
+        int steal = house[curr_index] + helper(house,curr_index+2 , dp);
+        int notSteal = helper(house,curr_index+1, dp);
+        return dp[curr_index]= max(steal, notSteal);
+     }
     // int helper(vector<int> &house,vector<int>&dp, int curr_index) {
     //     if (curr_index >= house.size() )
     //         return 0;
@@ -10,6 +18,7 @@ public:
     //     int skip = helper(house,dp, curr_index + 1);
     //     return dp[curr_index] = max(steal, skip);
     // }
+
     int tabulation(vector<int> &house, vector<int>& dp) {
         dp[0] = 0;
         int n = house.size();
@@ -24,6 +33,7 @@ public:
 
         int rob(vector<int> & nums) {
             vector<int> dp(101, -1);
-            return tabulation(nums, dp);
+            // return tabulation(nums, dp);
+            return helper(nums, 0,dp);
         }
     };
