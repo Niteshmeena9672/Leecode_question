efe93b08b62217cade6e77b9a48c6a941b1511e6
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2024-11-07 13:22:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (66.68%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    int climbStairs_memoization(int n, vector<int>& dp) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (dp[n] != -1)
-            return dp[n];
-        int one_step = climbStairs_memoization(n - 1, dp);
-        int two_step = climbStairs_memoization(n - 2, dp);
-        dp[n] = (one_step + two_step);
-        return dp[n];
+    int helper(int n , vector<int>&dp){
+         if(dp[n]!=-1) return dp[n];
+        if(n <=1) return 1;
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
     }
-    int climbStairs_tabulation(int n, vector<int>& dp) {
-        dp[0]=1;
-        dp[1]=1;
-        for(int i = 2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
-    }
-    int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return climbStairs_tabulation(n, dp);
+    int climbStairs(int n ) {
+        vector<int>dp(46,-1);
+        return helper(n,dp);
+       
     }
 };
